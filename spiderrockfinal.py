3dd2c96262002fa2325189256ec6b5eb6122f18a
nimishagupta2002 <nimisha.gupta2002@gmail.com>
2023-11-14 19:18:02 -0600

    Using List Operations and Commenting Answer

--- spiderrockfinal.py
+++ spiderrockfinal.py
@@ -19,20 +19,14 @@ def generate_permutations(row, remaining_width):
 def generate_conflict_matrix(floor_permutations, w):
     num_permutations = len(floor_permutations)
     
-    floor_sets = []
-    for floor in floor_permutations:
-        floor_sets.append(generate_set(floor, w))
+    floor_sets = [generate_set(floor, w) for floor in floor_permutations]
 
     # checks if there is overlap in the sets
-    conflict_matrix = []
-    temp = []
+    conflict_matrix = [[] for i in range(len(floor_permutations))]
     for i in range(num_permutations):
         for j in range(num_permutations):
             if (floor_sets[i].isdisjoint(floor_sets[j])):
-                temp.append(j)
-
-        conflict_matrix.append(temp)
-        temp = []
+                conflict_matrix[i].append(j)
 
     return conflict_matrix
 
@@ -57,16 +51,17 @@ def floor_problem(w, h):
 
     # uses dp & shallow copy to find the amount of floors possible
     dp_floors = num_permutations * [1]
-    temp = 0
+    temp_count = 0
     for i in range(1, h):
         temp_dp_floors = dp_floors.copy()
         for j in range(num_permutations):
             for k in conflict_matrix[j]:
-                temp += temp_dp_floors[k]
+                temp_count += temp_dp_floors[k]
             
-            dp_floors[j] = temp
-            temp = 0
+            dp_floors[j] = temp_count
+            temp_count = 0
 
     return sum(dp_floors)
 
-print(floor_problem(32, 10))
+print(floor_problem(9, 3)) #8
+print(floor_problem(32, 10)) #806844323190414
